3ab517daa65ac9bd6dba56ccb886c36ebdc5e883
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-15 02:57:26 -0500

    Add initial config - Add typescript support - Add vite config - Add eslint config - Add prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+  ],
+  plugins: ['simple-import-sort', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    eqeqeq: 'error',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'jsx-a11y/accessible-emoji': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# qr-code-generator
+# Simple Qr Code Generator
+
+## Installation
+
+Clone the repo and run `npm install`
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+Runs the app in development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### `npm run build`
+
+Builds the app for production to the `dist` folder.
+
+### `npm run serve`
+
+Tests the app locally, once you've built the app.
+
+### `npm run lint:format`
+
+Format the application with prettier.
+
+### `npm run lint:fix`
+
+Fixes the application's linting errors.
+
+### `npm run lint`
+
+Formats and fixes linting errors.
+
+### `npm run type-check`
+
+Checks for type errors.
