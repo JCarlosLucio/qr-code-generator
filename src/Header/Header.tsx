84160134a73e0fef1ad19b160765211f1d74c266
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-17 01:44:45 -0500

    Add text color to header - Change div to header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,9 @@
 export const Header = () => {
   return (
-    <div>
-      <p>QRCODE GENERATOR</p>
-    </div>
+    <header>
+      <h1 className="m-12 text-4xl text-black dark:text-white">
+        qrcode generator
+      </h1>
+    </header>
   );
 };
