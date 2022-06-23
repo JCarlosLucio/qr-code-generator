94414cb0dab46b5a7d789d52eccf6c4ba6037ddf
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-23 01:24:13 -0500

    Center title in header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header>
+    <header className="flex h-96 w-screen items-center justify-center">
       <h1 className="m-12 text-4xl text-black dark:text-white">
         qrcode generator
       </h1>
