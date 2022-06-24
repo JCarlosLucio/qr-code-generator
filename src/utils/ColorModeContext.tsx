4940c544e4d9dc1d71b3adb1d1eb6ce9d67cef41
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-24 01:34:34 -0500

    Add ColorModeContext/ColorModeProvider

---
+++ src/utils/ColorModeContext.tsx
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
+} from 'react';
+
+import { loadColorMode, saveColorMode } from './storage';
+import { ColorMode } from './types';
+
+const getInitialMode = (): ColorMode => {
+  const colorMode = loadColorMode();
+
+  if (colorMode) {
+    return colorMode;
+  }
+
+  if (window) {
+    const userMedia = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (userMedia.matches) {
+      return 'dark';
+    }
+  }
+  return 'light'; // defaults to light Mode
+};
+
+export const ColorModeContext = createContext<{
+  colorMode: ColorMode;
+  setColorMode: Dispatch<SetStateAction<ColorMode>>;
+}>({
+  colorMode: 'light',
+  setColorMode: () => {},
+});
+
+export const ColorModeProvider = ({ children }: { children: ReactNode }) => {
+  const [colorMode, setColorMode] = useState(getInitialMode);
+
+  const rawSetMode = (mode: ColorMode) => {
+    const root = window.document.documentElement;
+
+    root.classList.remove(mode === 'dark' ? 'light' : 'dark');
+    root.classList.add(mode);
+
+    saveColorMode(mode);
+  };
+
+  useEffect(() => {
+    rawSetMode(colorMode);
+  }, [colorMode]);
+
+  return (
+    <ColorModeContext.Provider value={{ colorMode, setColorMode }}>
+      {children}
+    </ColorModeContext.Provider>
+  );
+};
