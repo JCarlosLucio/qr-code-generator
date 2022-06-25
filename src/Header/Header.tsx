0795242cf6346a77bd70832560c20602fba2c7e9
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-25 02:17:35 -0500

    Add ColorModeToggle and GithubLink to Header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,12 @@
+import { ColorModeToggle } from './ColorModeToggle';
+import { GithubLink } from './GithubLink';
+
 export const Header = () => {
   return (
     <header className="flex h-44 w-80 items-center justify-center gap-4 sm:h-52 sm:w-screen">
       <h1 className="font-display text-2xl sm:text-4xl">QRCODE GENERATOR</h1>
+      <GithubLink />
+      <ColorModeToggle />
     </header>
   );
 };
