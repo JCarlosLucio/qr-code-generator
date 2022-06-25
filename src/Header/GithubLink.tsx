024432b8bdb94f8970e4b91b05bc81d0361e92f6
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-25 02:14:55 -0500

    Add GithubLink component

---
+++ src/Header/GithubLink.tsx
@@ -0,0 +1,9 @@
+import { FaGithubAlt } from 'react-icons/fa';
+
+export const GithubLink = () => {
+  return (
+    <a href="https://github.com/JCarlosLucio/qr-code-generator">
+      <FaGithubAlt className="text-3xl" />
+    </a>
+  );
+};
