dad833a56838cbbc803eef4236698517c7447a8a
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-25 02:14:06 -0500

    Add ColorModeToggle component

---
+++ src/Header/ColorModeToggle.tsx
@@ -0,0 +1,21 @@
+import { ChangeEvent, useContext } from 'react';
+import { FaRegMoon, FaRegSun } from 'react-icons/fa';
+import { ColorModeContext } from 'utils/ColorModeContext';
+
+export const ColorModeToggle = () => {
+  const { colorMode, setColorMode } = useContext(ColorModeContext);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setColorMode(event.target.checked ? 'dark' : 'light');
+  };
+
+  const isDark = colorMode === 'dark';
+
+  return (
+    <label className="swap swap-rotate">
+      <input type="checkbox" onChange={handleChange} checked={isDark} />
+      <FaRegMoon className="swap-on h-6 w-6 fill-current sm:h-6 sm:w-6" />
+      <FaRegSun className="swap-off h-6 w-6 fill-current sm:h-6 sm:w-6" />
+    </label>
+  );
+};
