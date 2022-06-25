420f851ae5a308eb62a32f455875e989d00f5947
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-25 02:22:32 -0500

    Style QRCodeGenerator component

--- src/QRCodeGenerator/QRCodeGenerator.tsx
+++ src/QRCodeGenerator/QRCodeGenerator.tsx
@@ -12,6 +12,8 @@ export const QRCodeGenerator = () => {
   const generate = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const dataUrl = await QRCode.toDataURL(url, {
+      width: 320,
+      margin: 1,
       type: 'image/png',
       color: {
         dark: '#00F', // Blue dots
@@ -23,22 +25,39 @@ export const QRCodeGenerator = () => {
   };
 
   return (
-    <main className="flex flex-col items-center">
-      <form>
-        <input
-          type="text"
-          value={url}
-          onChange={handleUrlChange}
-          placeholder="https://google.com"
-        />
-        <button type="submit" onClick={generate}>
+    <main className="mb-5 flex w-full max-w-xs flex-col items-center gap-4 sm:max-w-md">
+      <form className="form-control flex w-full flex-col items-center gap-4">
+        <label className="label" htmlFor="url-input">
+          <span className="label-text sm:text-lg">
+            Enter your link to generate the QR code
+          </span>
+        </label>
+        <div className="flex w-full max-w-xs flex-row items-center gap-4 sm:max-w-md">
+          <input
+            className="input input-bordered input-lg w-full"
+            type="text"
+            id="url-input"
+            value={url}
+            onChange={handleUrlChange}
+            placeholder="https://google.com"
+          />
+        </div>
+        <button
+          className="btn btn-primary btn-block"
+          type="submit"
+          onClick={generate}
+        >
           Generate
         </button>
       </form>
       {qrCode && (
         <>
           <img src={qrCode} alt="generated qr code" />
-          <a href={qrCode} download="qr-code.png">
+          <a
+            className="btn btn-secondary btn-block"
+            href={qrCode}
+            download="qr-code.png"
+          >
             Download
           </a>
         </>
