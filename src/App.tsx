e33ed73e02929f737a8bd2cb44646cf2d9bc6250
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-25 01:14:47 -0500

    Make background style of App component use bg from theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { QRCodeGenerator } from './QRCodeGenerator';
 
 const App = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-sky-100 bg-hero-pattern bg-cover text-slate-500 antialiased dark:bg-black dark:bg-dark-hero-pattern dark:text-slate-400">
+    <div className="flex min-h-screen flex-col items-center bg-hero-pattern bg-cover antialiased transition-all dark:bg-dark-hero-pattern">
       <Header />
       <QRCodeGenerator />
     </div>
