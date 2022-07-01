e22084f2c42ee76dca1b7669488be2219ee82dff
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-30 23:53:46 -0500

    Add WifiConfig interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,10 @@ export type ColorMode = 'light' | 'dark';
 export type TabKey = 'url' | 'wifi';
 
 export type Encryption = 'None' | 'WPA' | 'WEP';
+
+export interface WifiConfig {
+  ssid: string;
+  hiddenSSID: boolean;
+  password: string;
+  encryption: Encryption;
+}
