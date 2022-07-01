0d67d8584bdb101ce06fb8b8d3386cdddf319bde
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-30 22:42:51 -0500

    Add tabs for Url/Wifi generators

--- src/QRCodeGenerator/QRCodeGenerator.tsx
+++ src/QRCodeGenerator/QRCodeGenerator.tsx
@@ -1,18 +1,43 @@
 import { MouseEvent, useState } from 'react';
 import { FaDownload, FaPrint } from 'react-icons/fa';
+import { TabKey } from 'utils/types';
 
 import { UrlForm } from './UrlForm';
 
 export const QRCodeGenerator = () => {
+  const [activeTab, setActiveTab] = useState<TabKey>('url');
   const [qrCode, setQrCode] = useState<string>('');
 
   const handlePrint = (_event: MouseEvent<HTMLButtonElement>) => {
     window?.print();
   };
 
+  const handleTab =
+    (tabKey: TabKey) => (_event: MouseEvent<HTMLButtonElement>) => {
+      setActiveTab(tabKey);
+      setQrCode('');
+    };
+
+  const activeTabClass = (tabKey: TabKey) =>
+    activeTab === tabKey ? 'tab tab-active' : 'tab';
+
   return (
     <main className="mb-5 flex w-full max-w-xs flex-col items-center gap-4 sm:max-w-md print:max-w-2xl">
-      <UrlForm setQrCode={setQrCode} />
+      <div className="tabs tabs-boxed bg-base-100">
+        <button className={activeTabClass('url')} onClick={handleTab('url')}>
+          URL
+        </button>
+        <button className={activeTabClass('wifi')} onClick={handleTab('wifi')}>
+          WIFI
+        </button>
+      </div>
+
+      {activeTab === 'url' ? (
+        <UrlForm setQrCode={setQrCode} />
+      ) : (
+        <p>Wifi form</p>
+      )}
+
       {qrCode && (
         <>
           <img className="artboard-demo" src={qrCode} alt="generated qr code" />
