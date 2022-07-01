461d248fcdc86755a4358e0500dca0a1e49326f4
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-30 23:52:10 -0500

    Add base-200/base-300 colors to light/dark themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         light: {
           ...require('daisyui/src/colors/themes')['[data-theme=light]'],
           'base-100': '#FFFFFF',
+          'base-200': '#FFFFFF',
           'base-content': '#000000',
         },
       },
@@ -31,6 +32,8 @@ module.exports = {
         dark: {
           ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
           'base-100': '#000000',
+          'base-200': '#000000',
+          'base-300': '#111111',
           'base-content': '#FFFFFF',
         },
       },
