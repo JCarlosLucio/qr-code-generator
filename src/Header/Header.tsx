7e0e3a49fc63b54cef35c31ba14c9eda33416cfd
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-30 22:47:50 -0500

    Add margin top to Header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,7 +3,7 @@ import { GithubLink } from './GithubLink';
 
 export const Header = () => {
   return (
-    <header className="flex h-44 w-80 items-center justify-center gap-4 sm:h-52 sm:w-screen">
+    <header className="mt-8 flex h-28 w-80 items-center justify-center gap-4 sm:mt-14 sm:w-screen">
       <h1 className="font-display text-2xl sm:text-4xl">QRCODE GENERATOR</h1>
       <GithubLink />
       <ColorModeToggle />
