1aab914b6aaa9a5745b6e3b42f508df3d8ee143a
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-07-01 22:51:32 -0500

    Add generateWifiString

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,3 +1,5 @@
+import { WifiConfig } from './types';
+
 /**
  * Returns a string with special characters \ ; , " and : escaped with a backslash as in MECARD encoding.
  * @param str
@@ -10,4 +12,21 @@ export const mecardEncoding = (str: string): string => {
     .replace(/,/g, '\\,')
     .replace(/"/g, '\\"')
     .replace(/:/g, '\\:');
+};
+
+/**
+ * Generates a string from wi-fi configuration with the following syntax:
+ * WIFI:T:WPA;S:mynetwork;P:mypass;;
+ * @param config
+ * @returns
+ */
+export const generateWifiString = (config: WifiConfig): string => {
+  const { ssid, password, encryption, hiddenSSID } = config;
+
+  const s = `S:${mecardEncoding(ssid)};`;
+  const p = `P:${mecardEncoding(password)};`;
+  const t = encryption !== 'None' ? `T:${encryption};` : '';
+  const h = `H:${hiddenSSID};`;
+
+  return `WIFI:${s}${p}${h}${t};`;
 };
