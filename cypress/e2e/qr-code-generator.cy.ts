489383b70300f9a5ff893761ad32eb5b36baa43b
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-07-06 01:21:57 -0500

    Add a url qrcode can be created test

--- cypress/e2e/qr-code-generator.cy.ts
+++ cypress/e2e/qr-code-generator.cy.ts
@@ -12,4 +12,24 @@ describe('QRCODE GENERATOR APP', function () {
     cy.getByDataTest('generate-btn').should('exist');
     cy.getByDataTest('clear-btn').should('exist');
   });
+
+  describe('A URL QRCode can be created ', function () {
+    beforeEach(function () {
+      const urlInput = 'https://google.com';
+      cy.wrap(urlInput).as('urlInput');
+    });
+
+    it('Url qrcode is created from a valid url', function () {
+      const generateBtnSelector = 'generate-btn';
+
+      cy.getByDataTest('url-textarea')
+        .type(this.urlInput)
+        .should('have.value', this.urlInput);
+      cy.getByDataTest(generateBtnSelector).click();
+      cy.getByDataTest('qrcode-img').should('exist');
+      cy.getByDataTest('download-btn').should('exist');
+      cy.getByDataTest('print-btn').should('exist');
+      cy.getByDataTest(generateBtnSelector).should('be.disabled');
+    });
+  });
 });
