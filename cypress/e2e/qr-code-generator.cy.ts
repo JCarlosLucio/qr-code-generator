d0822f0e131b4a5bd4fd29f29519d170ee8943a6
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-07-06 01:20:48 -0500

    Extend Initial form is shown test to check more elements

--- cypress/e2e/qr-code-generator.cy.ts
+++ cypress/e2e/qr-code-generator.cy.ts
@@ -1,9 +1,15 @@
-describe('QRCODE GENERATOR APP', () => {
+describe('QRCODE GENERATOR APP', function () {
   beforeEach(function () {
     cy.visit('/');
   });
 
-  it('Inital Url form is shown', () => {
+  it('Inital Form is shown', function () {
     cy.contains('QRCODE GENERATOR');
+    cy.getByDataTest('url-tab').should('exist');
+    cy.getByDataTest('wifi-tab').should('exist');
+    cy.getByDataTest('github-link').should('exist');
+    cy.getByDataTest('color-mode-toggle').should('exist');
+    cy.getByDataTest('generate-btn').should('exist');
+    cy.getByDataTest('clear-btn').should('exist');
   });
 });
