4ee319ea80016d9e20205ae33571bbbc464de09c
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-07-10 19:55:21 -0500

    Change default preview port from 4173 to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: '/qr-code-generator/',
+  preview: {
+    port: 3000,
+  },
 });
