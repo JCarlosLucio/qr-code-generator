68bcae34e068596f05ce80fb6e87505211d7e275
Juan Carlos Lucio <c.lucio3@gmail.com>
2023-06-15 01:57:41 -0600

    Upgrade deps - Bump daisyui                            ^2.42.1  →    ^3.1.0 - Bump qrcode                              ^1.5.1  →    ^1.5.3 - Bump react-icons                         ^4.6.0  →    ^4.9.0 - Fix daisyui themes import

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,7 +23,7 @@ module.exports = {
     themes: [
       {
         light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          ...require('daisyui/src/theming/themes')['[data-theme=light]'],
           'base-100': '#FFFFFF',
           'base-200': '#FFFFFF',
           'base-content': '#000000',
@@ -31,7 +31,7 @@ module.exports = {
       },
       {
         dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
           'base-100': '#000000',
           'base-200': '#000000',
           'base-300': '#111111',
